87a5224295cb9c34afc5d5f401b4758cb98eaf32
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2022-02-24 17:31:35 +0100

    correzioni

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -8,18 +8,9 @@
     <link rel="stylesheet" href="{{asset("css/front.css")}}">
 </head>
 <body>
-    {{-- QUESTO ID=APP CI PERMETTE DI VEDERE LA WELCOME PAGE --}}
+
     <div id="app"></div>
-    {{-- @auth
-    <script>
-        window.autenticato = true;
-    </script>
-    @endauth
-    @guest
-    <script>
-        window.autenticato = false;
-    </script> 
-    @endguest --}}
+
     <script src="{{asset("js/front.js")}}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('front');
-});
+// Route::get('/', function () {
+//     return view('front');
+// });
 
 Auth::routes();
 
@@ -28,3 +28,7 @@ Route::prefix("admin")->namespace("Admin")->middleware("auth")->group(function()
     Route::resource("categories", "CategoryController");
     Route::resource("tags", "TagController");
 });
+
+Route::get("{any?}", function () {
+    return view('front');
+})->where ("any", ".*");
